6c5820fc23da6e27d7152b1439b2325c11e525fb
WonTaeJang <zmrdnjsxo@naver.com>
2019-01-28 12:33:56 +0900

    _map _filter _each js파일 추가

---
+++ js/_.js
@@ -0,0 +1,34 @@
+ // 2. _filter, _map으로 리팩토링
+ function _filter(users, predi) {       // 응용형 함수, 고차함수(함수를 인자로 받거나 함수를 리턴하거나)
+    var new_list = [];
+
+    _each(list, function(val){
+        if(predi(val)){new_list.push(val);}
+    });
+
+    // for(var i = 0; i < users.length; i++){
+    //     if(predi(users[i])){
+    //         new_list.push(users[i])
+    //     }
+    // }
+    return new_list;
+}
+
+function _map(list, mapper){
+    var new_list = [];
+    _each(list, function(val){      // 코드의 간결성
+        new_list.push(mapper(list[i]));
+    });
+    // for(var i = 0; i < list.length; i++){
+    //     new_list.push(mapper(list[i]));
+    // }
+    return new_list;
+}
+
+function _each(list, iter){
+    for(var i = 0; i < list.length; i++){
+        iter(list[i]);
+    }
+
+    return list
+}
