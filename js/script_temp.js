faeeb41aa087e4eab77614751feefe6569b9142a
WonTaeJang <zmrdnjsxo@naver.com>
2019-02-08 22:58:42 +0900

    test6.html 다형성 높이기 문장 추가 script_temp.js function add _go(), _pipe()

--- js/script_temp.js
+++ js/script_temp.js
@@ -12,19 +12,22 @@ var _get = _curryr(function(obj, key){
 function _filter(users, predi) {       // 응용형 함수, 고차함수(함수를 인자로 받거나 함수를 리턴하거나)
     var new_list = [];
 
-    _each(list, function(val){
+    // _each(list, function(val){
+
+    // });
+
+    for(var i = 0; i < users.length; i++){
         if(predi(users[i])){
             new_list.push(users[i])
         }
-    });
-
+    }
     return new_list;
 }
 
 function _map(list, mapper){
     var new_list = [];
     _each(list, function(val){
-        new_list.push(mapper(list[i]));
+        new_list.push(mapper(val));
     });
 
     return new_list;
@@ -42,11 +45,22 @@ var users = [
     {id:9, name: 'XX', age: 12}
 ];
 
+function _is_object(obj){
+    return typeof obj == 'object' && !!obj;
+}
+
+function _keys(obj){
+    return _is_object(obj) ? Object.keys(obj) : [];
+}
+
+
 var _length = _get('length');
 
 function _each(list, iter){     // list: array, iter: function
-    for(var i = 0, len = _length(list); i < len; i++){
-        iter(list[i]);
+    var keys = _keys(list);
+
+    for(var i = 0, len = keys.length; i < len; i++){
+        iter(list[keys[i]]);
     }
 
     return list
@@ -75,3 +89,19 @@ function _rest(list,num){
 // curryr이 적용된 맵과 filter
 var _map = _curryr(_map),
  _filter = _curryr(_filter);
+
+ 
+ function _pipe(){
+    var fns = arguments;
+    return function(arg){
+        return _reduce(fns, function(arg,fn){
+            return fn(arg);
+        }, arg);
+    }
+}
+
+ function _go(arg){
+    var fns = _rest(arguments); // array like object
+                                // 인자 값중 앞에 1을 제외한 나머지 함수 사용
+    return _pipe.apply(null, fns)(arg);
+}
